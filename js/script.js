1296069829fe2aed5fe02bef4630f5291e5bdb4d
Shivansh Singh (IN47812) <shivansh.10747812@ltimindtree.com>
2025-01-31 17:29:57 +0530

    Updated cursor functionality

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
+document.addEventListener("mouseleave", function () {
+  document.querySelector(".cursor").style.display = "none";
+});
+
+document.addEventListener("mouseenter", function () {
+  document.querySelector(".cursor").style.display = "block";
+});
+
 const cursorDot = document.querySelector("[data-cursor-dot]");
 const cursorOutline = document.querySelector("[data-cursor-outline]");
 
-window.addEventListener("mousemove", (e) =>{
-    const posX = e.clientX;
-    const posY = e.clientY;
+window.addEventListener("mousemove", (e) => {
+  const posX = e.clientX;
+  const posY = e.clientY;
 
-    cursorDot.style.left = `${posX}px`;
-    cursorDot.style.top = `${posY}px`;
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
 
-    cursorOutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    }, {duration: 100, fill: "forwards"})
-})
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 100, fill: "forwards" }
+  );
+});
